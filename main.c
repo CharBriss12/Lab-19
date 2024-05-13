f09796fc8fee64de48a554f50ee52eea3d11b8de
CharBriss12 <charlotbriss12@gmail.com>
2024-05-13 11:15:26 +0300

    lab19

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
-#include <stdio.h>
+#include <array/array.h>
+#include <set_bits/set_bits.h>
+#include <set_unordered/set_unordered.h>
+#include <set_ordered/set_ordered.h>
+#include <vector/vector.h>
+#include <vectors_void/vectors_void.h>
+#include <matrix/matrix.h>
+#include <string_/string_.h>
+#include <files/files.h>
+#include <test/test.h>
+
 
 int main() {
-    printf("Hello, World!\n");
+    //test_bitset();
+    //test_unordered_array_set();
+    //test_ordered_array_set();
+    //test_vectors();
+    //test_void_vector();
+    //testMatrix_1();
+    //testMatrix_2();
+    //test_string();
+    test_set();
+
     return 0;
 }
